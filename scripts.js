ec8f81a256ec9064726f1acc52a85213014dc2bd
Lilith Bentley <sbentley@gmail.com>
2019-09-01 14:30:39 -0500

    Create Employee object and employees array.

--- scripts.js
+++ scripts.js
@@ -1,6 +1,21 @@
 console.log('scripts.js loaded');
 $(document).ready(onReady);
 
+// employee constructor and array
+
+function Employee(firstName, lastName, emplId, emplTitle, salary) {
+  this.firstName = firstName;
+  this.lastName = lastName;
+  this.emplId = parseInt(emplId);
+  this.emplTitle = emplTitle;
+  this.salary = parseFloat(salary);
+} // end Employee
+
+let employees = [];
+employees.push(new Employee('Jen', 'Barber', 4521, 'Team Lead', 80000));
+employees.push(new Employee('Maurice', 'Moss', 8724, 'Support Team', 58000));
+employees.push(new Employee('Roy', 'Smith', 9623, 'Quality Assurance', 48000));
+
 // component functions
 
 
@@ -9,5 +24,6 @@ $(document).ready(onReady);
 
 function onReady() {
   console.log('jQuery onReady loaded');
+  console.log('employees:', employees);
   
 }
