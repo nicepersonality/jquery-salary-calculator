be2ec2892536cb0785e8d57a486cc75ad1a7d6d6
Lilith Bentley <sbentley@gmail.com>
2019-09-01 15:00:25 -0500

    Populate empl. table and monthly cost from array.

--- scripts.js
+++ scripts.js
@@ -18,7 +18,32 @@ employees.push(new Employee('Roy', 'Smith', 9623, 'Quality Assurance', 48000));
 
 // component functions
 
-
+function listEmployees(roster) {
+  // Replace the current contents of the employee table with the data
+  // supplied to the function (an array of Employees).
+  // Also, calculate the monthly total salary load.
+  let monthlySalary = 0;
+  $('#employeeTable tbody').empty();
+  for (employee of roster) {
+    let $tr = $('<tr></tr>');
+    $tr.append(`<td class="colFirstName">${employee.firstName}</td>`);
+    $tr.append(`<td class="colLastName">${employee.lastName}</td>`);
+    $tr.append(`<td class="colEmplId">${employee.emplId}</td>`);
+    $tr.append(`<td class="colEmplTitle">${employee.emplTitle}</td>`);
+    $tr.append(`<td class="colSalary">${employee.salary}</td>`);
+    $tr.append(`<td><button class="deleteRow">Delete</button></td>`);
+    $('#employeeTable tbody').append($tr);
+    monthlySalary += (employee.salary) / 12;
+    console.log('monthlySalary', monthlySalary);
+    
+  } // end for
+  $('#monthlyTotal').text(`${monthlySalary}`);
+  if (monthlySalary > 20000) {
+    $('#monthlyTotal').addClass('warning');
+  } else {
+    $('#monthlyTotal').removeClass('warning');
+  } // end if monthly total check
+} // end listEmployees
 
 // document ready function
 
